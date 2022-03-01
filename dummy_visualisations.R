ede989772d2cfbffc584e6911e462d9a007aabeb
Amanda Minter <amandalminter@gmail.com>
2022-03-01 11:22:04 +0000

    Added testing code

--- dummy_visualisations.R
+++ dummy_visualisations.R
@@ -1,7 +1,8 @@
 setwd("/Users/matthewgraham/Dropbox/LFProjCode")
+setwd("~/Documents/GitHub/") 
 source("vis_functions.R")
 
-#setwd("~/Documents/GitHub/") 
+
 cost_development <- 5000
 cost_cf <- 0.01
 cost_scenario <- 0.02
@@ -22,8 +23,23 @@ measure <- "WC" # output measure, WC : worm count
 elim <- "MF" # check if elimination was achieved before 2030
 
 ######## for one IU #######
+#### testing code DO NOT DELETE
 which_years <- c("Jan-2021","Jan-2022","Jan-2023","Jan-2024","Jan-2025","Jan-2026",
                  "Jan-2027","Jan-2028","Jan-2029","Jan-2030","Jan-2031")
+
+
+data_files <- read_files_def_cf(scenario, coverage, cf_non_compliance = "03", cf_coverage = "65", non_compliance,
+                                IU_order = 14, measure)
+
+# calculate relevant measuyre of impact (median at 2030)
+summary_res <- extract_medians(data_files, which_years)
+
+# what is the difference in WC and number of MDA rounds
+diff_measures <- summary_res$cf - summary_res$scenario
+
+# calculate relevant costs
+costs <- calculate_costs(summary_res, cost_scenario = 15, cost_development = 1000, cost_cf = 10, population = 1000)
+
 ######## across all IUs #######
 IUs <- read.csv("runIU.csv")
 IUs_vec <- which(IUs$IUID == 1)
@@ -156,13 +172,16 @@ IUs_stopped <- list(res_0$prop_IUs_Finished_MDA, res_1$prop_IUs_Finished_MDA,
                     res_3d$prop_IUs_Finished_MDA)
 labels <- c("0", "1", "2", "3a", "3b", "3c", "3d") # what to label blobs in same order as res_list
 
+##### trajectories
+# plot mean number of infections over time for each scenario
+make_time_plots(num_infs , labels, "number of infections")
+# plot mean number of IUs stopped MDA over time for each scenario
+make_time_plots(IUs_stopped , labels, "proportion of IUs stopped MDA")
+
+
 # original blob plot
 make_blob_plot(res_list, labels)
 
 # blob plot with net monetary elimination benefit colouring
 make_blob_plot_v2(res_list, labels, lambda_DALY = 10, lambda_EOT = 1)
 
-# plot mean number of infections over time for each scenario
-make_time_plots(num_infs , labels, "number of infections")
-# plot mean number of IUs stopped MDA over time for each scenario
-make_time_plots(IUs_stopped , labels, "proportion of IUs stopped MDA")
