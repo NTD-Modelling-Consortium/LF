8ab4bac4f5e2c6a726574ecfe0cb62d6d1b1da3b
Igor Clark <igor@igorclark.net>
2022-10-18 00:07:19 +0100

    add setup script to build model executable/runners

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# bail out immediately if anything errors
+set -euo pipefail
+
+# check for packaged GSL install
+if [[ -z $( which gsl-config ) ]] ; then
+	echo "=> error: please install 'gsl' using your system package manager ('brew install gsl' / 'apt-get install -y gsl-bin libgsl-dev')." >&2
+	exit 1
+fi
+
+# check for packaged pipenv install
+if [[ -z $( which pipenv ) ]] ; then
+	echo "=> error: please install 'pipenv' using your system package manager (homebrew/aptitude)" >&2
+	exit 1
+fi
+
+# do the C++ build
+echo "-> building C++ LF model ..."
+
+EXENAME="transfil_N"
+PROJECT_ROOT="${PWD}"
+RUN_DIR="${PROJECT_ROOT}/run"
+cd src
+make && ( [[ -f "${EXENAME}" ]] && cp "${EXENAME}" "${RUN_DIR}/${EXENAME}" ) && echo
+
+echo "-> checking for executable in ${RUN_DIR} ..."
+
+cd "${PROJECT_ROOT}"
+ls -l "${RUN_DIR}/${EXENAME}"
+echo
+
+# set up the python virtualenv
+echo "-> setting up python LF model runner ..."
+
+cd "${RUN_DIR}"
+pipenv install --dev
+pipenv run pip install .
+echo
+
+# extract data files
+echo "-> extracting scenario and parameter files ..."
+
+tar jxf parameters.tbz
+tar jxf scenarios.tbz
+ls -ld parameters scenarios
+cd "${PROJECT_ROOT}"
+cp src/Pop_Distribution.csv run
+echo
+
+echo "-> LF model and runner are built and ready to run."
