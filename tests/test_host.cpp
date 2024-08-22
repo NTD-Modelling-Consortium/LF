95e648eb7e97c23ab4b4728a0b9966951d6caac6
mattg3004 <matthewgraham3004@gmail.com>
2024-08-22 17:39:22 +0100

    update host test to test restore function

--- tests/test_host.cpp
+++ tests/test_host.cpp
@@ -1,10 +1,56 @@
 #include "Host.hpp"
 #include <catch2/catch_all.hpp>
-
+// cd build
+// cmake ..
+// cmake --build .
+// cd ..
+// ctest --test-dir build/tests
 TEST_CASE("Host", "[classic]") {
   SECTION("Host::restore") {
-    Host host;
-    hostState state;
-    host.restore(state);
+    // Initialize a population of hosts
+    int size = 10;
+    double TotalBiteRisk = 0.0;
+    Host host_pop[size];
+
+    // initialise population and set pTreat value
+    for (int i = 0; i < size; ++i) {
+        host_pop[i].initialise(
+            1.0, 100, 0.2, &TotalBiteRisk, 0.1, 0.1, 0.0);
+        host_pop[i].pTreat = (1.0 / i);
+    }
+
+    // Save the state of each host
+    hostState currentState[size];
+    for (int i = 0; i < size; ++i) {
+        currentState[i] = hostState(host_pop[i]);
+    }
+
+    // change pTreat, so that we know that the test is doing something
+    for (int i = 0; i < size; ++i) {
+        host_pop[i].pTreat = 1;  
+    }
+
+    // Restore the hosts from the saved state
+    for (int i = 0; i < size; ++i) {
+        host_pop[i].restore(currentState[i]);
+    }
+
+    // check that each host's state matches the saved state
+    for (int i = 0; i < size; ++i) {
+        REQUIRE(host_pop[i].WM == currentState[i].WM);
+        REQUIRE(host_pop[i].WF == currentState[i].WF);
+        REQUIRE(host_pop[i].totalWorms == currentState[i].totalWorms);
+        REQUIRE(host_pop[i].totalWormYears == currentState[i].totalWormYears);
+        REQUIRE(host_pop[i].M == currentState[i].M);
+        REQUIRE(host_pop[i].biteRisk == currentState[i].biteRisk);
+        REQUIRE(host_pop[i].age == currentState[i].age);
+        REQUIRE(host_pop[i].monthsSinceTreated == currentState[i].monthsSinceTreated);
+        REQUIRE(host_pop[i].hydroMult == currentState[i].hydroMult);
+        REQUIRE(host_pop[i].lymphoMult == currentState[i].lymphoMult);
+        REQUIRE(host_pop[i].sex == currentState[i].sex);
+        REQUIRE(host_pop[i].pTreat == 1.0/i);
+        REQUIRE(host_pop[i].pTreat == currentState[i].pTreat);
+    }
   }
 }
+
