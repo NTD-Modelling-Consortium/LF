ef1b5d986810ee6f160efab97a6d66c95da94444
Thomas <thomas.kiley@ndm.ox.ac.uk>
2024-08-22 14:56:35 +0100

    Fix formatting issues

--- tests/test_host.cpp
+++ tests/test_host.cpp
@@ -1,12 +1,10 @@
-#include <catch2/catch_all.hpp>
 #include "Host.hpp"
+#include <catch2/catch_all.hpp>
 
-TEST_CASE("Host", "[classic]")
-{
-    SECTION("Host::restore")
-    {
-        Host host;
-        hostState state;
-        host.restore(state);
-    }
+TEST_CASE("Host", "[classic]") {
+  SECTION("Host::restore") {
+    Host host;
+    hostState state;
+    host.restore(state);
+  }
 }
