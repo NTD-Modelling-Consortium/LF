470f5006d4e50ef0cb32a23004513e9316258241
mattg3004 <matthewgraham3004@gmail.com>
2024-08-22 17:41:30 +0100

    clang

--- tests/test_host.cpp
+++ tests/test_host.cpp
@@ -1,10 +1,6 @@
 #include "Host.hpp"
 #include <catch2/catch_all.hpp>
-// cd build
-// cmake ..
-// cmake --build .
-// cd ..
-// ctest --test-dir build/tests
+
 TEST_CASE("Host", "[classic]") {
   SECTION("Host::restore") {
     // Initialize a population of hosts
@@ -14,43 +10,42 @@ TEST_CASE("Host", "[classic]") {
 
     // initialise population and set pTreat value
     for (int i = 0; i < size; ++i) {
-        host_pop[i].initialise(
-            1.0, 100, 0.2, &TotalBiteRisk, 0.1, 0.1, 0.0);
-        host_pop[i].pTreat = (1.0 / i);
+      host_pop[i].initialise(1.0, 100, 0.2, &TotalBiteRisk, 0.1, 0.1, 0.0);
+      host_pop[i].pTreat = (1.0 / i);
     }
 
     // Save the state of each host
     hostState currentState[size];
     for (int i = 0; i < size; ++i) {
-        currentState[i] = hostState(host_pop[i]);
+      currentState[i] = hostState(host_pop[i]);
     }
 
     // change pTreat, so that we know that the test is doing something
     for (int i = 0; i < size; ++i) {
-        host_pop[i].pTreat = 1;  
+      host_pop[i].pTreat = 1;
     }
 
     // Restore the hosts from the saved state
     for (int i = 0; i < size; ++i) {
-        host_pop[i].restore(currentState[i]);
+      host_pop[i].restore(currentState[i]);
     }
 
     // check that each host's state matches the saved state
     for (int i = 0; i < size; ++i) {
-        REQUIRE(host_pop[i].WM == currentState[i].WM);
-        REQUIRE(host_pop[i].WF == currentState[i].WF);
-        REQUIRE(host_pop[i].totalWorms == currentState[i].totalWorms);
-        REQUIRE(host_pop[i].totalWormYears == currentState[i].totalWormYears);
-        REQUIRE(host_pop[i].M == currentState[i].M);
-        REQUIRE(host_pop[i].biteRisk == currentState[i].biteRisk);
-        REQUIRE(host_pop[i].age == currentState[i].age);
-        REQUIRE(host_pop[i].monthsSinceTreated == currentState[i].monthsSinceTreated);
-        REQUIRE(host_pop[i].hydroMult == currentState[i].hydroMult);
-        REQUIRE(host_pop[i].lymphoMult == currentState[i].lymphoMult);
-        REQUIRE(host_pop[i].sex == currentState[i].sex);
-        REQUIRE(host_pop[i].pTreat == 1.0/i);
-        REQUIRE(host_pop[i].pTreat == currentState[i].pTreat);
+      REQUIRE(host_pop[i].WM == currentState[i].WM);
+      REQUIRE(host_pop[i].WF == currentState[i].WF);
+      REQUIRE(host_pop[i].totalWorms == currentState[i].totalWorms);
+      REQUIRE(host_pop[i].totalWormYears == currentState[i].totalWormYears);
+      REQUIRE(host_pop[i].M == currentState[i].M);
+      REQUIRE(host_pop[i].biteRisk == currentState[i].biteRisk);
+      REQUIRE(host_pop[i].age == currentState[i].age);
+      REQUIRE(host_pop[i].monthsSinceTreated ==
+              currentState[i].monthsSinceTreated);
+      REQUIRE(host_pop[i].hydroMult == currentState[i].hydroMult);
+      REQUIRE(host_pop[i].lymphoMult == currentState[i].lymphoMult);
+      REQUIRE(host_pop[i].sex == currentState[i].sex);
+      REQUIRE(host_pop[i].pTreat == 1.0 / i);
+      REQUIRE(host_pop[i].pTreat == currentState[i].pTreat);
     }
   }
 }
-
