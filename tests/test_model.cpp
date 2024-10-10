7264d09c285d64526992151ac73b6aa8daf07afb
mattg3004 <matthewgraham3004@gmail.com>
2024-10-10 11:20:55 +0800

    Update test_model.cpp

--- tests/test_model.cpp
+++ tests/test_model.cpp
@@ -33,8 +33,8 @@ TEST_CASE("Model", "[classic]") {
 
     SECTION("Repeat previous test, but also test whether time is divisible by "
             "12, and as it isn't, this should be false") {
-      REQUIRE_FALSE(!model.shouldReduceImportationViaPrevalance(1, t, 270) &&
-                    (t % 12 == 0));
+      REQUIRE_FALSE((!model.shouldReduceImportationViaPrevalance(1, t, 270) &&
+                    (t % 12 == 0)));
     }
   }
 }
